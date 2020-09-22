9b17704822ad2c81eaf5f724dc17cceb40003673
Cameron Logie <cameronlogie24@gmail.com>
2020-09-22 15:23:08 +0100

    made array into hash and added cohort month

--- directory.rb
+++ directory.rb
@@ -1,17 +1,18 @@
 #put students into an array
 students = [
-   "Dr. Hannibal Lecter",
-   "Darth Vader",
-   "Nurse Ratched",
-   "Michael Corleone",
-   "Alex DeLarge",
-   "The Wicked Witch of the West",
-   "Terminator",
-   "Freddy Krueger",
-   "The Joker",
-   "Joffrey Baratheon",
-   "Norman Bates"
- ]
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
 def print_header
   puts "The Students of Villains Academy"
   puts "---------------"
@@ -19,7 +20,7 @@ end
 
 def print(names)
   names.each do |names|
-    puts names
+    puts "#{names[:name]} (#{names[:cohort]}) cohort"
   end
 end
 
